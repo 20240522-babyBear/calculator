d50285c4172d42cc38e727a4bf8a5bb762792ad6
yanghyeyeon <yhy4058@gmail.com>
2024-05-22 17:03:09 +0900

    메소드 전체 수정
    메소드 수정과 스캔을 사용한 빼기 연산 코드 작성

--- src/main/java/com/babyBear/calculator/Yanghyeyeon.java
+++ src/main/java/com/babyBear/calculator/Yanghyeyeon.java
@@ -1,9 +1,28 @@
 package com.babyBear.calculator;
 
+import java.util.Scanner;
+
 public class Yanghyeyeon {
 
-    public static double minus(double num1, double num2) {
+    public static void minus() {
 
-        return num1 - num2 ;
+        double result = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("더하기 할 첫번째 값을 입력해주세요 : ");
+        double num1 = sc.nextDouble();
+        System.out.print("더하기 할 두번째 값을 입력해주세요 : ");
+        double num2 = sc.nextDouble();
+
+        result = num1 - num2;
+
+        System.out.println("계산결과는 '" + result + "' 입니다.");
+
+        System.out.println("추가로 원하시는 기능의 이름 입력하세요 : ");
+
+
+
+
     }
 }
