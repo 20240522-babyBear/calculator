00b010b702d55d7ac25e846f3aa0fb35a2bcc47e
wdh970616 <wdh970616@naver.com>
2024-05-22 17:07:24 +0900

    메소드 수정
    main Application을 간략화 하기 위한
    추가적인 기능 추가

--- src/main/java/com/babyBear/calculator/Seongmin.java
+++ src/main/java/com/babyBear/calculator/Seongmin.java
@@ -1,11 +1,20 @@
 package com.babyBear.calculator;
 
-public class Seongmin {
-    public static double divide(double num1, double num2) {
-        return num1 / num2;
-    }
+import java.util.Scanner;
 
-    public static double remainder(double num1, double num2) {
-        return num1 % num2;
+public class Seongmin {
+    public static void divide() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("피제수를 입력해주세요 : ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("제수를 입력해주세요 : ");
+        double num2 = sc.nextDouble();
+
+        System.out.println();
+        System.out.println("나눈 몫은 '" + (num1 / num2) + "' 이고 나머지는 '" + (num1 % num2) + "' 입니다.");
+        System.out.println();
+        System.out.println("추가로 원하시는 기능의 이름을 입력하세요 : ");
     }
 }
