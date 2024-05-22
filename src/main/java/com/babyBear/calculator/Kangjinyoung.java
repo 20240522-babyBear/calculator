9672c2dcc2357b945b253bde13b2b98013ac6042
weed97 <weed9935@gmail.com>
2024-05-22 13:22:37 +0900

    재수정 곱셈기능 메서드

--- src/main/java/com/babyBear/calculator/Kangjinyoung.java
+++ src/main/java/com/babyBear/calculator/Kangjinyoung.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Kangjinyoung {
 
-    public  double  multiple(double num1, double num2) {
+    public  static double  multiple(double num1, double num2) {
 
         return num1 * num2;
 
