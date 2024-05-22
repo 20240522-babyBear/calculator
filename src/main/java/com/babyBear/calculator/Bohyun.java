9879cd0a57f869c7c54d77de273854d3b684d6ba
weed97 <weed9935@gmail.com>
2024-05-22 17:56:44 +0900

    불필요한 어플리케이션 삭제

--- src/main/java/com/babyBear/calculator/Bohyun.java
+++
@@ -1,8 +0,0 @@
-package com.babyBear.calculator;
-
-public class Bohyun {
-    public static double plus(double num1, double num2) {
-        return num1 + num2;
-    }
-
-}
--- src/main/java/com/babyBear/calculator/Yanghyeyeon.java
+++
@@ -1,9 +0,0 @@
-package com.babyBear.calculator;
-
-public class Yanghyeyeon {
-
-    public static double minus(double num1, double num2) {
-
-        return num1 - num2 ;
-    }
-}
