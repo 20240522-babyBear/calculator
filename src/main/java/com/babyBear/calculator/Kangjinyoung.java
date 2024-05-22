4fa19a41ce3550f10e050655b6f24f5d0f9989e8
weed97 <weed9935@gmail.com>
2024-05-22 13:15:44 +0900

    곱셈기능 메서드

---
+++ src/main/java/com/babyBear/calculator/Kangjinyoung.java
@@ -0,0 +1,14 @@
+package com.babyBear.calculator;
+
+import java.util.Scanner;
+
+public class Kangjinyoung {
+
+    public  double  multiple(double num1, double num2) {
+
+        return num1 * num2;
+
+
+
+    }
+}
