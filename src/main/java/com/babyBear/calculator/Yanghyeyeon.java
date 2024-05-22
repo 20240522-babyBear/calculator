a1824e84c5aa82eda1c6e2d08b925742ace9454d
yanghyeyeon <yhy4058@gmail.com>
2024-05-22 13:23:58 +0900

    메소드 작성
    뺄셈 메소드 작성

--- src/main/java/com/babyBear/calculator/Yanghyeyeon.java
+++ src/main/java/com/babyBear/calculator/Yanghyeyeon.java
@@ -2,7 +2,7 @@ package com.babyBear.calculator;
 
 public class Yanghyeyeon {
 
-    public static double minusNumber(double num1, double num2) {
+    public static double minus(double num1, double num2) {
 
         return num1 - num2;
     }
