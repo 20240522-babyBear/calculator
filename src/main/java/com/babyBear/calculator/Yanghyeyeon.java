422f5359cb9d8af6a518a90bd3d42e22918811b1
yanghyeyeon <yhy4058@gmail.com>
2024-05-22 17:10:59 +0900

    내용수정
    result 값을 빼고 num1 - num2로 변경 한 후 위아래로 줄바꿈 넣어줌

--- src/main/java/com/babyBear/calculator/Yanghyeyeon.java
+++ src/main/java/com/babyBear/calculator/Yanghyeyeon.java
@@ -6,8 +6,6 @@ public class Yanghyeyeon {
 
     public static void minus() {
 
-        double result = 0;
-
         Scanner sc = new Scanner(System.in);
 
         System.out.print("더하기 할 첫번째 값을 입력해주세요 : ");
@@ -15,9 +13,9 @@ public class Yanghyeyeon {
         System.out.print("더하기 할 두번째 값을 입력해주세요 : ");
         double num2 = sc.nextDouble();
 
-        result = num1 - num2;
-
-        System.out.println("계산결과는 '" + result + "' 입니다.");
+        System.out.println();
+        System.out.println("계산결과는 '" + (num1 + num2) + "' 입니다.");
+        System.out.println();
 
         System.out.println("추가로 원하시는 기능의 이름 입력하세요 : ");
 
