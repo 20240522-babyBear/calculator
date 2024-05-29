74ada4451808a1fd3dfc4c6336f4697b83cf2861
weed97 <weed9935@gmail.com>
2024-05-29 21:32:38 +0900

    api

---
+++ Ex_Code/src/main/java/exsersice/review/create/object/exapi/Application.java
@@ -0,0 +1,8 @@
+package exsersice.review.create.object.exapi;
+
+public class Application {
+    public static void main(String[] args) {
+
+        StringProcess sp = new StringProcess();
+    }
+}
---
+++ Ex_Code/src/main/java/exsersice/review/create/object/exapi/StringProcess.java
@@ -0,0 +1,29 @@
+package exsersice.review.create.object.exapi;
+
+public class StringProcess {
+
+    public String  prChar(String s) {
+        s = "hello world";
+
+        s = s.toUpperCase();
+
+
+
+
+
+
+
+        return s;
+
+
+
+}
+
+    public void charSu(String s, char ch) {
+
+
+
+
+
+    }
+}
